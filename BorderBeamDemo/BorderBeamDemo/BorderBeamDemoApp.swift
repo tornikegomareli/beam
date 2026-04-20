c84aaf8b259d57a88597894c8150c00801cea66b
Tornike Gomareli <gomarelidevelopment@gmail.com>
2026-04-20 10:27:23 +0400

    scaffold demo app

---
+++ BorderBeamDemo/BorderBeamDemo/BorderBeamDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  BorderBeamDemoApp.swift
+//  BorderBeamDemo
+//
+//  Created by Tornike Gomareli on 19.04.26.
+//
+
+import SwiftUI
+
+@main
+struct BorderBeamDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
